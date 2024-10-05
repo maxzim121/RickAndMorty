9f8cc4dec8e9e0f82906ebed7422b97d4fb75450
Maksim <122309695+maxzim121@users.noreply.github.com>
2024-10-05 23:26:09 +0300

    Revert "Feature/coordinator"

--- RickAndMorty/SceneDelegate.swift
+++ RickAndMorty/SceneDelegate.swift
@@ -17,11 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
+        
         window = UIWindow(windowScene: scene)
-        let navigationController = UINavigationController()
-        window?.rootViewController = navigationController
-        let coordinator = AppCoordinator(navigationController: navigationController)
-        coordinator.start()
+        let viewController = ViewController()
+        window?.rootViewController = viewController
         window?.makeKeyAndVisible()
         
     }
---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Maksim Zimens on 03.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
