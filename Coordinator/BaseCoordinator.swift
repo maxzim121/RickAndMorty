e90163902b2155c19d1d434462841c8acd475188
maxzim121 <zimensm12@gmail.com>
2024-10-05 23:12:51 +0300

    Добавил и настроил Coordinator: mainFlow с двумя координаторами

--- Coordinator/BaseCoordinator.swift
+++ Coordinator/BaseCoordinator.swift
@@ -1,8 +1,30 @@
-//
-//  BaseCoordinator.swift
-//  RickAndMorty
-//
-//  Created by Maksim Zimens on 05.10.2024.
-//
+import UIKit
 
-import Foundation
+class BaseCoordinator: CoordinatorProtocol {
+    var finishDelegate: CoordinatorFinishDelegate?
+    var navigationController: UINavigationController
+    var childCoordinators: [CoordinatorProtocol]
+
+    init(
+        finishDelegate: CoordinatorFinishDelegate? = nil,
+        navigationController: UINavigationController,
+        childCoordinators: [CoordinatorProtocol] = [CoordinatorProtocol]()
+    ) {
+        self.finishDelegate = finishDelegate
+        self.navigationController = navigationController
+        self.childCoordinators = childCoordinators
+    }
+
+    deinit {
+        childCoordinators.forEach { $0.finishDelegate = nil }
+        childCoordinators.removeAll()
+    }
+
+    func start() {
+        print("Coordinator start")
+    }
+
+    func finish() {
+        print("Coordinator finish")
+    }
+}
--- MainScreen/MainScreenViewController.swift
+++ MainScreen/MainScreenViewController.swift
@@ -7,22 +7,38 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class MainScreenViewController: UIViewController {
     
     // MARK: - Проверка сервиса
     
+    var coordinator: MainScreenCoordinator
+    
     let network = NetworkClient.shared
-
+    
+    var timer: Timer?
+    
+    init(coordinator: MainScreenCoordinator) {
+        self.coordinator = coordinator
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchPage()
-        fetchCharacter()
         
+        view.backgroundColor = .red
+//        fetchPage()
+//        fetchCharacter()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false){ [weak self] _ in
+            self?.coordinator.showNextScreen()}
     }
     
     func fetchPage() {
         network.fetchPage(pageId: 1) { [weak self] result in
-            guard let self = self else { return }
+            guard self != nil else { return }
             switch result {
             case .success(let response):
                 print(response)
@@ -34,7 +50,7 @@ final class ViewController: UIViewController {
     
     func fetchCharacter() {
         network.fetchCharacter(id: 1) { [weak self] result in
-            guard let self = self else { return }
+            guard self != nil else { return }
             switch result {
             case .success(let response):
                 print(response)
