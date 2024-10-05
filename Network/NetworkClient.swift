2d0c19244dec20de3c77bf75679a288be2061574
maxzim121 <zimensm12@gmail.com>
2024-10-05 17:36:04 +0300

    Добавлен запрос на одну страницу (20 персонажей)

--- Network/NetworkClient.swift
+++ Network/NetworkClient.swift
@@ -1,8 +1,32 @@
-//
-//  NetworkClient.swift
-//  RickAndMorty
-//
-//  Created by Maksim Zimens on 05.10.2024.
-//
+import UIKit
 
-import Foundation
+typealias PageResponseCompletion = (Result<PageResponse, Error>) -> Void
+
+final class NetworkClient {
+    private let urlSession = URLSession.shared
+    private var pageTask: URLSessionTask?
+    static let shared = NetworkClient()
+    
+    func fetchPage(completion: @escaping PageResponseCompletion) {
+        assert(Thread.isMainThread)
+        let request = pageRequest()
+        pageTask = urlSession.object(urlSession: urlSession, for: request) { [weak self] (result: Result<PageResponse, Error>) in
+            DispatchQueue.main.async {
+                guard self != nil else {return}
+                switch result {
+                case .success(let page):
+                    completion(.success(page))
+                case .failure(let error):
+                    completion(.failure(error))
+                    assertionFailure("\(error)")
+                }
+            }
+        }
+    }
+    
+    func pageRequest() -> URLRequest {
+        URLRequest.makeHTTPRequest(
+            httpMethod: "get"
+        )
+    }
+}
--- Network/ResponceStruct.swift
+++ Network/ResponceStruct.swift
@@ -1,8 +1,33 @@
-//
-//  ResponceStruct.swift
-//  RickAndMorty
-//
-//  Created by Maksim Zimens on 05.10.2024.
-//
+struct PageResponse: Codable {
+    let info: Info
+    let results: [Results]
+}
 
-import Foundation
+struct Info: Codable {
+    let count: Int
+    let pages: Int
+    let next: String?
+    let prev: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case count
+        case pages
+        case next
+        case prev
+    }
+}
+
+struct Results: Codable {
+    let id: Int
+    let name: String
+    let gender: String
+    let image: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case gender
+        case image
+    }
+}
+
